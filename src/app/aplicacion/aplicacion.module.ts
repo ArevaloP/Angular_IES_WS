ba08d8b5246826c3cd218f058a2a4b5e48459b78
Andres Humberto Villamizar <andreshv2@gmail.com>
2019-07-16 21:17:56 -0500

    'actualizacion02'

--- src/app/aplicacion/aplicacion.module.ts
+++ src/app/aplicacion/aplicacion.module.ts
@@ -7,7 +7,7 @@ import { ServiciowebComponent } from './componente/servicioweb/servicioweb.compo
 import { LisAppExternaComponent } from './componente/aplicacionext/lis-app-externa/lis-app-externa.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AddAppExternaComponent } from './componente/aplicacionext/add-app-externa/add-app-externa.component';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 
 @NgModule({
@@ -22,7 +22,9 @@ import { AddAppExternaComponent } from './componente/aplicacionext/add-app-exter
   imports: [
     CommonModule,
     AplicacionRoutingModule,
-    BsDropdownModule
+    BsDropdownModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AplicacionModule { }
--- src/app/aplicacion/componente/aplicacionext/add-app-externa/add-app-externa.component.ts
+++ src/app/aplicacion/componente/aplicacionext/add-app-externa/add-app-externa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AplicacionExterna } from '../../../modelo/aplicacion-externa';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RestAplicacionService } from '../../../servicio/rest-aplicacion.service';
 
 @Component({
   selector: 'app-add-app-externa',
@@ -10,16 +11,32 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AddAppExternaComponent implements OnInit {
 
 
-  public aplicacionExterna:AplicacionExterna=new AplicacionExterna();
-  fGeneral: FormGroup;
-  
-  constructor(private fb: FormBuilder  ) {
+  public aplicacionExterna: AplicacionExterna = new AplicacionExterna();
+  private fGeneral: FormGroup;
+  private isModificar: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private restAplicacion: RestAplicacionService
 
-   }
+  ) {
+
+  }
 
   ngOnInit() {
     this.inicializarValidacion();
+
+
+    if (this.restAplicacion.getAplicacionExterna() != null) {
+      this.aplicacionExterna = this.restAplicacion.getAplicacionExterna();
+      this.isModificar = true;
+    } else {
+      this.aplicacionExterna.estado = "ACTIVO";
+      this.aplicacionExterna.tipo = "WEB";
+      this.isModificar = false;
+    }
+
+
   }
 
 
@@ -27,16 +44,50 @@ export class AddAppExternaComponent implements OnInit {
 
     //console.log(this.fb);
     this.fGeneral = this.fb.group({
-      codigo: [this.aplicacionExterna.codigo||'@', [Validators.required]]/*,
+      //codigo: [this.aplicacionExterna.codigo, [Validators.required]]
+      codigo: [this.aplicacionExterna.codigo, Validators.required],
       nombre: [this.aplicacionExterna.nombre, Validators.required],
-      urlAplicacion: [this.aplicacionExterna.urlAplicacion, Validators.required],*/
+      urlAplicacion: [this.aplicacionExterna.urlAplicacion, Validators.required],
+      estado: [this.aplicacionExterna.estado, Validators.required],
+      tipo: [this.aplicacionExterna.tipo, Validators.required],
+      descripcion: [this.aplicacionExterna.descripcion, Validators.required]
     }
     );
 
   }
 
 
-  public registarAplicacionExterna(){
+  public registarAplicacionExterna() {
+    // alert(JSON.stringify(this.aplicacionExterna));
+
+    //this.aplicacionExterna.registradoPor = this.usuarioVO.id;
+    //this.aplicacionExterna.usuarioRealiza = this.usuarioVO.nombre;
+    //this.aplicacionExterna.aplxFechaCambio = new Date().toLocaleString();
+    //this.aplicacionExterna.listaServicio = this.procesarListaServicioAdd();
+    //this.aplicacionExterna.listaUsuario = this.procesarListaUsuarioAdd();
+
+    if (this.isModificar) {
+      console.log(this.aplicacionExterna.id);
+      this.restAplicacion.actualizarAplicacionExterna(this.aplicacionExterna).subscribe(
+        data => {
+          console.log("actualizacion realizada correctamente !!!")
+          //this.router.navigate(['principal/aplicacion']);
+        },
+        error => { console.log("falla la actualizacion del registro !!!") }
+      )
+    } else {
+      //this.aplicacionExterna.aplxColor = "gray";
+      //this.aplicacionExterna.aplxIcono = "ion ion-person-add";
+      ///this.aplicacionExterna.aplxFechaCreacion = new Date().toLocaleString();
+
+      this.restAplicacion.insertarAplicacionExterna(this.aplicacionExterna).subscribe(
+        data => {
+          console.log("insercion realizada correctamente !!!")
+          //this.router.navigate(['principal/aplicacion']);
+        },
+        error => { console.log("falla la insercion del registro !!!") }
+      )
+    }
 
 
   }
