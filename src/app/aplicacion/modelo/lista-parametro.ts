91df658fbe7aab5aed0161c952498d4c45385a28
CIADTI <desarrollounipamplona@gmail.com>
2019-10-04 11:12:19 -0500

    Ajuste en validación de combo lista de parámetros para el tipo de dato array en funcionalidad Servicios web -> Parámetros.

---
+++ src/app/aplicacion/modelo/lista-parametro.ts
@@ -0,0 +1,10 @@
+export class ListaParametro
+{
+    public id: String;
+    public codigo: String;
+    public nombre: String;
+    public estado: String;
+    public usuarioRealiza: String;
+    public ip: String;
+    public registradoPor: String;
+}
