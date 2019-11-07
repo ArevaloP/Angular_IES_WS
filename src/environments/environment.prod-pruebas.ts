33ec37b76c1850d2d2d829d1ab2352dd0adc5cb1
andreshv2 <andreshv2@gmail.com>
2019-11-07 16:10:45 -0500

    cambios json parametros

---
+++ src/environments/environment.prod-pruebas.ts
@@ -0,0 +1,27 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  grupo:'integrador-panamericana',
+  baseUrl: 'https://siafpruebas.unipanamericana.edu.co:8190/integrador-rest/servicios/'  ,
+  configuracionMsal :{
+    clientID: '986be348-7575-496d-8485-cf7971c3660d',
+    authority: "https://login.microsoftonline.com/4bf38ea2-832d-4552-b508-421570da43ff/",
+    validateAuthority: true,
+    redirectUri: "https://siafpruebas.unipanamericana.edu.co:8190/integrador-web/load",
+    cacheLocation: "sessionStorage",
+    postLogoutRedirectUri: "https://siafpruebas.unipanamericana.edu.co:8190/integrador-web",
+    navigateToLoginRequestUrl: false,
+    popUp: false,
+    consentScopes: [
+    ],
+    correlationId: '1234',
+    piiLoggingEnabled: true,
+    unprotectedResources: ["https://siafpruebas.unipanamericana.edu.co:8190/integrador-rest/servicios/"]
+  },
+  optiosMsal:[
+  ]
+};
