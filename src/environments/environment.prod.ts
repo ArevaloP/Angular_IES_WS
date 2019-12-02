a8428cce5e6e9029b6ede2b5e60ab9d2319e2414
jmsierra26 <sierra_jesus1@hotmail.com>
2019-12-02 17:50:04 -0500

    Ajuste de json de parámetros para producción.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -30,13 +30,16 @@ export const environment = {
     production: false,
     grupo:'integradorRestWs-pruebas',
     baseUrl: 'https://test.ciadti.co:9024/integrador-rest/servicios/'  ,
+    // baseUrl: 'http://localhost:9019/integrador-rest/servicios/',
     configuracionMsal :{
       clientID: 'ff27df78-09ee-4f0a-9dea-ccb5b21abe81',
       authority: "https://login.microsoftonline.com/2fe9a652-eedf-4168-9370-18a597068a8e/",
       validateAuthority: true,
       redirectUri: "https://test.ciadti.co:9024/integrador-web/load",
+      // redirectUri: "http://localhost:9019/integrador-web/load",
       cacheLocation: "sessionStorage",
       postLogoutRedirectUri: "https://test.ciadti.co:9024/integrador-web",
+      // postLogoutRedirectUri: "http://localhost:9019/integrador-web",
       navigateToLoginRequestUrl: false,
       popUp: false,
       consentScopes: [
@@ -44,6 +47,7 @@ export const environment = {
       correlationId: '1234',
       piiLoggingEnabled: true,
       unprotectedResources: ["https://test.ciadti.co:9024/integrador-rest/servicios/"]
+      // unprotectedResources: ["http://localhost:9019/integrador-rest/servicios/"]
     },
     optiosMsal:[
     ]
