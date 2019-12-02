2119190d3cc420128671625e9aadea1220c464d4
jmsierra26 <sierra_jesus1@hotmail.com>
2019-12-02 11:14:20 -0500

    Cambio de direcciones.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
+/*
 export const environment = {
   production: true,
   grupo:'integrador-panamericana',
@@ -24,4 +24,27 @@ export const environment = {
   },
   optiosMsal:[
   ]
-};
+};*/
+
+export const environment = {
+    production: false,
+    grupo:'integradorRestWs-pruebas',
+    baseUrl: 'https://test.ciadti.co:9024/integrador-rest/servicios/'  ,
+    configuracionMsal :{
+      clientID: 'ff27df78-09ee-4f0a-9dea-ccb5b21abe81',
+      authority: "https://login.microsoftonline.com/2fe9a652-eedf-4168-9370-18a597068a8e/",
+      validateAuthority: true,
+      redirectUri: "https://test.ciadti.co:9024/integrador-web/load",
+      cacheLocation: "sessionStorage",
+      postLogoutRedirectUri: "https://test.ciadti.co:9024/integrador-web",
+      navigateToLoginRequestUrl: false,
+      popUp: false,
+      consentScopes: [
+      ],
+      correlationId: '1234',
+      piiLoggingEnabled: true,
+      unprotectedResources: ["https://test.ciadti.co:9024/integrador-rest/servicios/"]
+    },
+    optiosMsal:[
+    ]
+  };
