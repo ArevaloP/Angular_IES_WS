ca22104d891f979b476182051d3d101cc6be4aab
Andres H Villamizar <andreshv2@gmail.com>
2019-12-05 16:38:10 -0500

    cambios en sincronizacion de pagos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-<<<<<<< HEAD
-=======
 
 
->>>>>>> desa-ahv
 /*
 export const environment = {
   production: true,
